24caac89e9b18556e48577df12c4e022cd0166b4
yuki011121 <tyy17723818165@126.com>
2025-03-03 00:18:25 +0000

    Formatted training script and updated config file

---
+++ RPPO_modified/training.py
@@ -0,0 +1,146 @@
+import os
+import yaml
+import json
+import argparse
+
+from diambra.arena import load_settings_flat_dict, SpaceTypes
+from diambra.arena.stable_baselines3.make_sb3_env import (
+    make_sb3_env,
+    EnvironmentSettings,
+    WrappersSettings,
+)
+from diambra.arena.stable_baselines3.sb3_utils import linear_schedule, AutoSave
+
+# import RecurrentPPO
+from sb3_contrib.ppo_recurrent import RecurrentPPO, MultiInputLstmPolicy
+
+
+def main(cfg_file):
+    # Read the cfg file
+    yaml_file = open(cfg_file)
+    params = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    print("Config parameters = ", json.dumps(params, sort_keys=True, indent=4))
+    yaml_file.close()
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    model_folder = os.path.join(
+        base_path,
+        params["folders"]["parent_dir"],
+        params["settings"]["game_id"],
+        params["folders"]["model_name"],
+        "model",
+    )
+    tensor_board_folder = os.path.join(
+        base_path,
+        params["folders"]["parent_dir"],
+        params["settings"]["game_id"],
+        params["folders"]["model_name"],
+        "tb",
+    )
+
+    os.makedirs(model_folder, exist_ok=True)
+
+    # Settings
+    params["settings"]["action_space"] = (
+        SpaceTypes.DISCRETE
+        if params["settings"]["action_space"] == "discrete"
+        else SpaceTypes.MULTI_DISCRETE
+    )
+    settings = load_settings_flat_dict(EnvironmentSettings, params["settings"])
+
+    # Wrappers Settings
+    wrappers_settings = load_settings_flat_dict(
+        WrappersSettings, params["wrappers_settings"]
+    )
+
+    # Create environment
+    env, num_envs = make_sb3_env(settings.game_id, settings, wrappers_settings)
+    print("Activated {} environment(s)".format(num_envs))
+
+    # Policy param
+    policy_kwargs = params["policy_kwargs"]
+
+    # PPO settings
+    ppo_settings = params["ppo_settings"]
+    gamma = ppo_settings["gamma"]
+    model_checkpoint = ppo_settings["model_checkpoint"]
+
+    learning_rate = linear_schedule(
+        ppo_settings["learning_rate"][0], ppo_settings["learning_rate"][1]
+    )
+    clip_range = linear_schedule(
+        ppo_settings["clip_range"][0], ppo_settings["clip_range"][1]
+    )
+    clip_range_vf = clip_range
+    batch_size = ppo_settings["batch_size"]
+    n_epochs = ppo_settings["n_epochs"]
+    n_steps = ppo_settings["n_steps"]
+
+    # if use LSTM，adjust batch_size/n_steps
+    if model_checkpoint == "0":
+        # Initialize the agent (train from scratch)
+        # 改用 RecurrentPPO + MultiInputLstmPolicy
+        agent = RecurrentPPO(
+            policy=MultiInputLstmPolicy,
+            env=env,
+            verbose=1,
+            gamma=gamma,
+            batch_size=batch_size,
+            n_epochs=n_epochs,
+            n_steps=n_steps,
+            learning_rate=learning_rate,
+            clip_range=clip_range,
+            clip_range_vf=clip_range_vf,
+            policy_kwargs=policy_kwargs,
+            tensorboard_log=tensor_board_folder,
+            ent_coef=params["ppo_settings"].get("ent_coef", 0.0),
+        )
+    else:
+        # Load the trained agent
+        agent = RecurrentPPO.load(
+            os.path.join(model_folder, model_checkpoint),
+            env=env,
+            gamma=gamma,
+            learning_rate=learning_rate,
+            clip_range=clip_range,
+            clip_range_vf=clip_range_vf,
+            policy_kwargs=policy_kwargs,
+            tensorboard_log=tensor_board_folder,
+        )
+
+    # Print policy network architecture
+    print("Policy architecture:")
+    print(agent.policy)
+
+    # Create the callback: autosave every USER DEF steps
+    autosave_freq = ppo_settings["autosave_freq"]
+    auto_save_callback = AutoSave(
+        check_freq=autosave_freq,
+        num_envs=num_envs,
+        save_path=model_folder,
+        filename_prefix=model_checkpoint + "_",
+    )
+
+    # Train the agent
+    time_steps = ppo_settings["time_steps"]
+    agent.learn(total_timesteps=time_steps, callback=auto_save_callback)
+
+    # Save the agent
+    new_model_checkpoint = str(int(model_checkpoint) + time_steps)
+    model_path = os.path.join(model_folder, new_model_checkpoint)
+    agent.save(model_path)
+
+    # Close the environment
+    env.close()
+
+    # Return success
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfgFile", type=str, required=True, help="Configuration file")
+    opt = parser.parse_args()
+    print(opt)
+
+    main(opt.cfgFile)
